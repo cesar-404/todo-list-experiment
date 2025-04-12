899b7c00c8e7c1d2a399960f07f6a092772d69c4
Cesar Oliveira <cesarhenriqoliveira@gmail.com>
2025-04-12 18:42:57 -0300

    :sparkles: mappers classes

--- src/main/java/br/com/bebopcode/todolist/service/mappers/TaskMapper.java
+++ src/main/java/br/com/bebopcode/todolist/service/mappers/TaskMapper.java
@@ -1,4 +1,47 @@
 package br.com.bebopcode.todolist.service.mappers;
 
+import br.com.bebopcode.todolist.dto.TaskRequest;
+import br.com.bebopcode.todolist.dto.TaskResponse;
+import br.com.bebopcode.todolist.dto.TaskUpdateResponse;
+import br.com.bebopcode.todolist.model.Task;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class TaskMapper {
+
+    public Task toEntity(TaskRequest taskRequest) {
+        return new Task(taskRequest.title(),
+                taskRequest.description(),
+                taskRequest.priority(),
+                taskRequest.status());
+    }
+
+    public TaskResponse toTaskResponse(Task task) {
+        return new TaskResponse(task.getTitle(),
+                task.getDescription(),
+                task.getPriority(),
+                task.getStatus(),
+                task.getCreatedAt());
+    }
+
+    public TaskResponse toTaskResponse(Optional<Task> task) {
+        var taskResult = task.orElseThrow(
+                () -> new RuntimeException("Task not found")
+        );
+        return toTaskResponse(taskResult);
+    }
+
+    public TaskUpdateResponse toTaskUpdateResponse(Optional<Task> task) {
+        var taskResult = task.orElseThrow(
+                () -> new RuntimeException("Task not found"));
+
+        return new TaskUpdateResponse(taskResult.getTitle(),
+                taskResult.getDescription(),
+                taskResult.getPriority(),
+                taskResult.getStatus(),
+                taskResult.getCreatedAt(),
+                taskResult.getUpdatedAt());
+    }
 }
