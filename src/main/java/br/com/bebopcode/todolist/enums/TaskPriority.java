eeacdcb67b8bab1ad98f1c987656c376c0cdd71c
Cesar Oliveira <cesarhenriqoliveira@gmail.com>
2025-04-12 18:41:16 -0300

    :sparkles: enums classes

--- src/main/java/br/com/bebopcode/todolist/enums/TaskPriority.java
+++ src/main/java/br/com/bebopcode/todolist/enums/TaskPriority.java
@@ -1,4 +1,18 @@
 package br.com.bebopcode.todolist.enums;
 
 public enum TaskPriority {
+
+    HIGH(1),
+    NORMAL(2),
+    LOW(3);
+
+    private final int order;
+
+    TaskPriority(int order) {
+        this.order = order;
+    }
+
+    public int getOrder() {
+        return order;
+    }
 }
--- src/main/java/br/com/bebopcode/todolist/enums/TaskStatus.java
+++ src/main/java/br/com/bebopcode/todolist/enums/TaskStatus.java
@@ -1,4 +1,12 @@
 package br.com.bebopcode.todolist.enums;
 
 public enum TaskStatus {
+
+    ACTIVE,
+    COMPLETED,
+    IN_PROGRESS,
+    CANCELLED,
+    FAILED,
+    INACTIVE,
+
 }
